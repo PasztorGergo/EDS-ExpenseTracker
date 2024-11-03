f44d79b556e5392f9ca529f51b6c05b902301b6b
Pásztor Gergő <pasztorg05@gmail.com>
2024-11-03 10:33:11 +0100

    Initial commit

---
+++ src/test/java/Enginear/eds/ExpenseTracker/AppTest.java
@@ -0,0 +1,19 @@
+package Enginear.eds.ExpenseTracker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
